6a7171b1b76f21ebf76dc4412a261a74df03c2b0
Justin <Justin@192.168.1.48>
2020-12-15 16:27:44 +0100

    insert user

--- src/fr/eni/BLL/Manager.java
+++ src/fr/eni/BLL/Manager.java
@@ -7,10 +7,14 @@ import fr.eni.DAL.DALException;
 import fr.eni.DAL.articleDao;
 import fr.eni.DAL.DaoFactory;
 import fr.eni.BO.Articles;
+import fr.eni.BO.Utilisateurs;
 
 public class Manager {
 
 	articleDao methode = new DaoFactory().getdao();
+	
+//----------------affichage--list--article--------------
+	
 	public List<Articles> getliste() throws DALException{
 		List<Articles> liste = new ArrayList<Articles>();
 		try {
@@ -20,5 +24,12 @@ public class Manager {
 			e.printStackTrace();
 		}
 		return liste;
+	}
+	
+//---------------insert--user----------------------------
+	
+	public void add_user(Utilisateurs user) throws DALException
+	{		
+		methode.insert(user); 
 	}
 }
