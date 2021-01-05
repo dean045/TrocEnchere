427c1013d05d8c0a579963eebd4dc4bfc581fd65
Nico <nicolasleclerc2020@campus-eni.fr>
2021-01-05 11:45:31 +0100

    Class Catégories

---
+++ src/fr/eni/BO/Categories.java
@@ -0,0 +1,31 @@
+package fr.eni.BO;
+
+public class Categories {
+	
+	private int no_categorie; 
+	private String libelle ; 
+	
+	public Categories (int no_categorie, String libelle) {
+		this.no_categorie = no_categorie ; 
+		this.libelle = libelle ; 
+	}
+
+	public int getNo_categorie() {
+		return no_categorie;
+	}
+
+	public void setNo_categorie(int no_categorie) {
+		this.no_categorie = no_categorie;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	
+	
+
+}
