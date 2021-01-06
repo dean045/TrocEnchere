6d653b688c3dab0c50e1fbb5bfc4cabce6582233
Justin <Justin@192.168.1.48>
2021-01-06 13:40:36 +0100

    modif article

--- src/fr/eni/Servlet/modifierArticle.java
+++ src/fr/eni/Servlet/modifierArticle.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpSession;
 import fr.eni.BLL.Manager;
 import fr.eni.BO.Articles;
 import fr.eni.BO.Categories;
-import fr.eni.BO.Utilisateurs;
 import fr.eni.DAL.DALException;
 
 /**
@@ -27,6 +26,7 @@ public class modifierArticle extends HttpServlet {
 	private RequestDispatcher rd;
 	private HttpSession session;
 	Manager manager = new Manager();
+	String message;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		rd = request.getRequestDispatcher("WEB-INF/modifierArticle.jsp");
@@ -36,48 +36,47 @@ public class modifierArticle extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		session = request.getSession();
-		int noArticle = Integer.valueOf(request.getParameter("button").substring(1));
-
-
+		
+		Articles article = new Articles();
+		
 		if(request.getParameter("button").charAt(0) == 'm'){
-			Articles article = new Articles();
+			int noArticle = Integer.valueOf(request.getParameter("button").substring(1));
 			List<Categories> listeCat = new ArrayList<Categories>();
-
+			
 			try {
 				article = manager.getArticle(noArticle);
 				listeCat = manager.getLibelle();
 				request.setAttribute("article", article);
 				request.setAttribute("listeCat", listeCat);
 			} catch (DALException e) {
-
+			
 				e.printStackTrace();
 			}
-
-
+			
+			
 			rd = request.getRequestDispatcher("WEB-INF/modifierArticle.jsp");
 			rd.forward(request, response);
 		}
-
-
-
+		
 		else { 
 
-			noArticle = Integer.valueOf(request.getParameter("button").substring(1));
+            noArticle = Integer.valueOf(request.getParameter("button").substring(1));
 
-			if (request.getParameter("button").charAt(0) == 's') {
-				
-				try {
-					manager.delete_art(noArticle);
-					rd = request.getRequestDispatcher("index");
-					rd.forward(request, response);
-					
-				} catch (Exception e) {
+            if (request.getParameter("button").charAt(0) == 's') {
 
-					e.printStackTrace();
-					doGet(request, response);
-				}
-			}
-		}
+                try {
+                    manager.delete_art(noArticle);
+                    rd = request.getRequestDispatcher("index");
+                    rd.forward(request, response);
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+                    doGet(request, response);
+                }
+            }
+        }
+	
 		
 		else if(request.getParameter("button").charAt(0) == 'c') {
 			System.out.println("essai 1");
@@ -117,4 +116,3 @@ public class modifierArticle extends HttpServlet {
 		}
 	}
 }
-
